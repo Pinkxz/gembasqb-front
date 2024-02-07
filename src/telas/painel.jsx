ff6acd4e1a7bfec4e101867144a8fb1aa62c581f
pinkxz <victor666157@gmail.com>
2024-02-07 17:05:29 -0300

    painel

---
+++ src/telas/painel.jsx
@@ -0,0 +1,17 @@
+function toggleProfileOptions() {
+    var profileOptions = document.getElementById("profileOptions");
+    if (profileOptions.style.display === "none") {
+        profileOptions.style.display = "block";
+    } else {
+        profileOptions.style.display = "none";
+    }
+}
+
+function toggleNavbar() {
+    var navbar = document.getElementById("navbar");
+    if (navbar.style.display === "none") {
+        navbar.style.display = "block";
+    } else {
+        navbar.style.display = "none";
+    }
+}
