d07284fd59dc804995d9742320b92d2e37bb835c
pinkxz <victor666157@gmail.com>
2024-02-09 16:45:35 -0300

    funcoes

--- src/telas/services.jsx
+++ src/telas/services.jsx
@@ -86,3 +86,32 @@ function adicionarItem() {
         container.style.overflowY = "scroll";
     }
 }
+
+function toggleProfileOptions() {
+    var profileOptions = document.getElementById("profileOptions");
+    if (profileOptions.style.display === "none") {
+        profileOptions.style.display = "block";
+    } else {
+        profileOptions.style.display = "none";
+    }
+  }
+  
+  function toggleNavbar() {
+    var navbar = document.getElementById("navbar");
+    var overlay = document.getElementById("overlay");
+    if (navbar.style.left === "-200px") {
+        navbar.style.left = "0";
+        overlay.style.display = "block";
+    } else {
+        navbar.style.left = "-200px";
+        overlay.style.display = "none";
+    }
+  }
+  
+  /* Função para fechar a navbar */
+  function closeNavbar() {
+    var navbar = document.getElementById("navbar");
+    var overlay = document.getElementById("overlay");
+    navbar.style.left = "-200px";
+    overlay.style.display = "none";
+  }
