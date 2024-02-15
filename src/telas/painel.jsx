eb6283793d16c3f3d03b5475f4708d0227e5888f
pinkxz <victor666157@gmail.com>
2024-02-15 20:07:39 -0300

    popup e container

--- src/telas/painel.jsx
+++ src/telas/painel.jsx
@@ -6,3 +6,21 @@ function toggleProfileOptions() {
         profileOptions.style.display = "none";
     }
 }
+
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
+function cancelar() {
+    closePopup();
+    // Implemente a lógica para cancelar aqui
+}
+
+function abrirCaixa() {
+    // Implemente a lógica para abrir o caixa aqui
+    closePopup();
+}
