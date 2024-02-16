91d0161cd1455def19a58bda56460433b716c206
pinkxz <victor666157@gmail.com>
2024-02-16 16:20:12 -0300

    modal

--- src/telas/painel.jsx
+++ src/telas/painel.jsx
@@ -7,6 +7,15 @@ function toggleProfileOptions() {
     }
 }
 
+window.onload = function () {
+    var caixaAberto = localStorage.getItem("caixaAberto");
+
+    if (caixaAberto === "true") {
+        document.getElementById("caixaFechado").style.display = "none";
+        document.getElementById("frenteCaixa").style.display = "block";
+    }
+}
+
 function openPopup() {
     document.getElementById("popup").style.display = "block";
 }
@@ -16,6 +25,55 @@ function closePopup() {
 }
 
 function abrirCaixa() {
-    // Implemente a lógica para abrir o caixa aqui
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var time = hours + ":" + (minutes < 10 ? "0" : "") + minutes;
+    document.getElementById("abertura").innerText = "Aberto em: " + now.toLocaleDateString() + " " + time;
+    document.getElementById("caixaFechado").style.display = "none";
+    document.getElementById("frenteCaixa").style.display = "block";
+    localStorage.setItem("caixaAberto", "true");
     closePopup();
+}
+
+function fecharCaixa() {
+    document.getElementById("caixaFechado").style.display = "block";
+    document.getElementById("frenteCaixa").style.display = "none";
+    localStorage.removeItem("caixaAberto");
+}
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+// Função para fechar a modal
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+/*========================================================================Funcionamento Front-End acima==================================================*/
+
+function preencherClientesSelect() {
+    var select = $("#clientesSelect");
+
+    // Limpa o select antes de adicionar as opções
+    select.empty();
+
+    // Adiciona uma opção vazia (se necessário)
+    $("<option>").text("Selecione um cliente").appendTo(select);
+
+    // Faz uma solicitação AJAX para obter os clientes do servidor
+    $.ajax({
+        url: "/clientes",
+        type: "GET",
+        dataType: "json",
+        success: function(clientes) {
+            $.each(clientes, function(i, cliente) {
+                $("<option>").val(cliente.id).text(cliente.nome).appendTo(select);
+            });
+        },
+        error: function(xhr, status, error) {
+            console.error("Erro ao obter clientes:", status, error);
+        }
+    });
 }
