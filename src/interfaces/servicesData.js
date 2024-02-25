fdd6fa4beb5a23c5fee8427d88b80cc26e202785
pinkxz <victor666157@gmail.com>
2024-02-25 18:55:39 -0300

    hook

---
+++ src/interfaces/servicesData.js
@@ -0,0 +1,12 @@
+export const servicesData = {
+    idServico: '',
+    nomeServico: '',
+    descricao: '',
+    categoria: '',
+    preco: 0,
+    tempo: 0.0,
+    status: '',
+    imagem: '',
+    comissao: 0,
+    fidelidade: ''
+};
